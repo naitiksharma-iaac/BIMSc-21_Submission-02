54eab5144c3ac0f0e907d7dfa720aec1c21a192d
Naitik Sharma <naitik.sharma@students.iaac.net>
2022-01-24 22:46:00 +0530

    Submission-02
    Contains a welcome page and two separate pages showing two separate pages

---
+++ example1.js
@@ -0,0 +1,46 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124.0/build/three.module.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/controls/OrbitControls.js'
+import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/3DMLoader.js'
+
+let scene, camera, renderer
+const model = 'bldg-parts.3dm'
+
+init()
+animate()
+
+
+function init () {
+
+    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 )
+
+
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(1,1,1)
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+    camera.position.y = - 30
+
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } )
+    renderer.setSize( window.innerWidth, window.innerHeight )
+    document.body.appendChild( renderer.domElement )
+
+    const controls = new OrbitControls( camera, renderer.domElement )
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff )
+    directionalLight.intensity = 2
+    scene.add( directionalLight )
+
+    const loader = new Rhino3dmLoader()
+    loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
+
+    loader.load( model, function ( object ) {
+        scene.add( object )
+    } )
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate )
+    renderer.render( scene, camera )
+}
